11112e5cd120acd87d55f929684bc237e2ab0a12
Mariyan Yordanov <marsrewq@gmail.com>
2022-04-16 19:24:26 +0300

    Add files via upload

---
+++ FirstPrograms/ForLoops/EqualPairs/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _0008._Equal_Pairs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int maxDiff = int.MinValue;
+            bool check = false;
+            int diff = 0;
+            for (int i = 1; i <= number; i++)
+            {
+                
+                int a = int.Parse(Console.ReadLine());
+                int b = int.Parse(Console.ReadLine());
+                sum = a + b;
+                if (number == 1)
+                {
+                    check = true;
+                }
+                if (sum == maxDiff)
+                {
+                    check = true;
+                    diff = sum;
+                }
+                if (maxDiff > sum)
+                {
+                    check = false;
+                    diff = maxDiff - sum;
+                }
+                if (maxDiff < sum)
+                {
+                    check = false;
+                    diff = sum - maxDiff;
+                }
+                diff += maxDiff;
+            }
+            if (check == true)
+            {
+                Console.WriteLine($"Yes, value={sum}");
+            }
+            else
+            {
+                Console.WriteLine($"No, maxdiff={diff}");   
+            }
+        }
+    }
+}
