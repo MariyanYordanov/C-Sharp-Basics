b0632cfed314a508101d1caa0b45221ad477ce3e
Mariyan Yordanov <marsrewq@gmail.com>
2022-04-17 22:11:38 +0300

    Add files via upload

---
+++ FirstPrograms/WhileLoops/PrimeNumberCheck/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PrimeNumberCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            bool check = true;
+            int divider = 2;
+            int dividerMax = (int)Math.Sqrt(number);
+            
+            while (check && (divider <= dividerMax))
+            {
+                if (number % divider == 0)
+                {
+                    check = false;
+                }
+                divider++;
+            }
+            if (check)
+            {
+                Console.WriteLine($"Number {number} is a prime.");
+            }
+            else
+            {
+                Console.WriteLine($"Number {number} is not a prime.");
+            }
+        }
+    }
+}
