4e35c3d927d767b7b24460f746c55bfb8b5163e5
Mariyan Yordanov <marsrewq@gmail.com>
2022-04-18 21:32:54 +0300

    Add lessons

---
+++ FirstPrograms/lessons/FluelTankTwo/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+
+namespace FluelTankTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fluelType = Console.ReadLine();
+            double quantity = int.Parse(Console.ReadLine());
+            string card = Console.ReadLine();
+
+            if (card == "No")
+            {
+                if ((quantity > 20) && (quantity <= 25))
+                {
+                    if (fluelType == "Gasoline")
+                    {
+                        double coust = (quantity * 2.22) - (quantity * 2.22 * 0.08);
+                        Console.WriteLine($"{coust:f2} lv.");
+                    }
+                    else if (fluelType == "Diesel")
+                    {
+                        double coust = (quantity * 2.33) - (quantity * 2.33 * 0.08);
+                        Console.WriteLine($"{coust:f2} lv.");
+                    }
+                    else if (fluelType == "Gas")
+                    {
+                        double coust = (quantity * 0.93) - (quantity * 0.93 * 0.08);
+                        Console.WriteLine($"{coust:f2} lv.");
+                    }
+                }
+                else if (quantity > 25)
+                {
+                    if (fluelType == "Gasoline")
+                    {
+                        double coust = (quantity * 2.22) - (quantity * 2.22 * 0.1);
+                        Console.WriteLine($"{coust:f2} lv.");
+                    }
+                    else if (fluelType == "Diesel")
+                    {
+                        double coust = (quantity * 2.33) - (quantity * 2.33 * 0.1);
+                        Console.WriteLine($"{coust:f2} lv.");
+                    }
+                    else if (fluelType == "Gas")
+                    {
+                        double coust = (quantity * 0.93) - (quantity * 0.93 * 0.1);
+                        Console.WriteLine($"{coust:f2} lv.");
+                    }
+                }
+                else
+                {
+                    if (fluelType == "Gasoline")
+                    {
+                        double coust = quantity * 2.22 ;
+                        Console.WriteLine($"{coust:f2} lv.");
+                    }
+                    else if (fluelType == "Diesel")
+                    {
+                        double coust = quantity * 2.33;
+                        Console.WriteLine($"{coust:f2} lv.");
+                    }
+                    else if (fluelType == "Gas")
+                    {
+                        double coust = quantity * 0.93;
+                        Console.WriteLine($"{coust:f2} lv.");
+                    }
+                }
+            }
+            else if (card == "Yes")
+            {
+                if ((quantity > 20) && (quantity <= 25))
+                {
+                    if (fluelType == "Gasoline")
+                    {
+                        double coust = (quantity * 2.04) - (quantity * 2.04 * 0.08);
+                        Console.WriteLine($"{coust:f2} lv.");
+                    }
+                    else if (fluelType == "Diesel")
+                    {
+                        double coust = (quantity * 2.21) - (quantity * 2.21 * 0.08);
+                        Console.WriteLine($"{coust:f2} lv.");
+                    }
+                    else if (fluelType == "Gas")
+                    {
+                        double coust = (quantity * 0.85) - (quantity * 0.85 * 0.08);
+                        Console.WriteLine($"{coust:f2} lv.");
+                    }
+                }
+                else if (quantity > 25)
+                {
+                    if (fluelType == "Gasoline")
+                    {
+                        double coust = (quantity * 2.04) - (quantity * 2.04 * 0.1);
+                        Console.WriteLine($"{coust:f2} lv.");
+                    }
+                    else if (fluelType == "Diesel")
+                    {
+                        double coust = (quantity * 2.21) - (quantity * 2.21 * 0.1);
+                        Console.WriteLine($"{coust:f2} lv.");
+                    }
+                    else if (fluelType == "Gas")
+                    {
+                        double coust = (quantity * 0.85) - (quantity * 0.85 * 0.1);
+                        Console.WriteLine($"{coust:f2} lv.");
+                    }
+                }
+                else
+                {
+                    if (fluelType == "Gasoline")
+                    {
+                        double coust = (quantity * 2.04);
+                        Console.WriteLine($"{coust:f2} lv.");
+                    }
+                    else if (fluelType == "Diesel")
+                    {
+                        double coust = (quantity * 2.21);
+                        Console.WriteLine($"{coust:f2} lv.");
+                    }
+                    else if (fluelType == "Gas")
+                    {
+                        double coust = (quantity * 0.85);
+                        Console.WriteLine($"{coust:f2} lv.");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ FirstPrograms/lessons/Harvester/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Harvester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sqM = int.Parse(Console.ReadLine());
+            double grapeY = double.Parse(Console.ReadLine());
+            int vineZ = int.Parse(Console.ReadLine());
+            int numWorkers = int.Parse(Console.ReadLine());
+
+            double totalharvestInLitters = ((sqM * grapeY) * 0.4) / 2.5;
+
+            if (totalharvestInLitters < vineZ)
+            {
+                Console.WriteLine($"It will be a tough winter! More {Math.Floor(vineZ -totalharvestInLitters)} liters wine needed.");
+            }
+            else if (totalharvestInLitters >= vineZ)
+            {
+                double enoughWine = totalharvestInLitters - vineZ;
+                double vineForOneWorker = enoughWine / numWorkers;
+
+                Console.WriteLine($"Good harvest this year! Total wine: {Math.Floor(totalharvestInLitters)} liters.");
+
+                Console.WriteLine($"{Math.Ceiling(enoughWine)} liters left -> {Math.Ceiling(vineForOneWorker)} liters per person.");
+
+            }
+        }
+    }
+}
