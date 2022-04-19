f619cadb62fd7ad6b418db247e434edf7c33ddf8
Mariyan Yordanov <marsrewq@gmail.com>
2022-04-19 12:29:56 +0300

    New simple program commit

---
+++ FirstPrograms/ConditionalStatements/FruitMarket/FruitMarket/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FruitMarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lenghtTank = int.Parse(Console.ReadLine());
+            int widthTank = int.Parse(Console.ReadLine());
+            int heightTank = int.Parse(Console.ReadLine());
+            double porcent = double.Parse(Console.ReadLine());
+
+            double volumeWhithoutPorcent = (lenghtTank * widthTank * heightTank) * 0.001;
+            double diferens = volumeWhithoutPorcent - (volumeWhithoutPorcent * (porcent * 0.01));
+
+            Console.WriteLine(diferens);
+        }
+    }
+}
