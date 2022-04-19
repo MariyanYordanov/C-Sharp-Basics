ac835393fcf63cb46e629fca044205b84e5c9efb
Mariyan Yordanov <marsrewq@gmail.com>
2022-04-19 12:51:44 +0300

    Add Calc Deposit

---
+++ FirstPrograms/VeryFirstCode/CalcDeposit/calcDeposit/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace calcDeposit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double deposit = double.Parse(Console.ReadLine());
+            int depositPeriod = int.Parse(Console.ReadLine());
+            double depositYearPorcent = double.Parse(Console.ReadLine());
+
+            double dividend = deposit * depositYearPorcent / 100;
+            double dividendPerMonth = dividend / 12;
+
+
+            double sum = deposit + (dividendPerMonth * depositPeriod);
+            Console.WriteLine(sum);
+        }
+    }
+}
