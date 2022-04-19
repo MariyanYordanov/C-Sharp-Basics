ddd62bcd7937074588e2cf80eadc9d804990c6ad
Mariyan Yordanov <marsrewq@gmail.com>
2022-04-19 12:49:17 +0300

    Add Back To The Past and Charity Campaign

---
+++ FirstPrograms/Lessons/BackToThePast/Program.cs
@@ -0,0 +1,52 @@
+﻿
+
+using System;
+ 
+namespace _01._Back_To_The_Past
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double inheritance = double.Parse(Console.ReadLine());
+            int year = int.Parse(Console.ReadLine());
+
+            double expences = 0;
+            double totalExpences = 0;
+            int age = 18;
+            double moneyLeft = 0;
+            for (int i = 1800; i <= year; i++)
+            {
+                if (i % 2 == 0)
+                {
+
+                    expences = 12000;
+                    totalExpences = inheritance - expences;
+
+                    moneyLeft += totalExpences;
+
+                }
+                else
+                {
+                    expences = 12000 + 50 * (age + 1);
+
+                    moneyLeft = totalExpences - expences;
+
+                }
+            }
+            double diff = Math.Abs(moneyLeft - inheritance);
+
+            if (moneyLeft <= inheritance)
+            {
+                Console.WriteLine($"Yes! He will live a carefree life and will have {diff:f2} dollars left.");
+            }
+            else
+            {
+                Console.WriteLine($"He will need {diff:f2} dollars to survive.");
+            }
+        }
+    }
+}
+
+
+
---
+++ FirstPrograms/VeryFirstCode/CharityCampaign/CharityCampaign/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CharityCampaign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfDays = int.Parse(Console.ReadLine());
+            int numberOfBakers = int.Parse(Console.ReadLine());
+            int numberOfCakes = int.Parse(Console.ReadLine());
+            int numberOfWaffles = int.Parse(Console.ReadLine());
+            int numberOfPancakes = int.Parse(Console.ReadLine());
+
+            double priceCakaesPerDay = 45 * numberOfCakes;
+            double priceWafflesPerDay = 5.80 * numberOfWaffles;
+            double pricePancakesPerDay = 3.20 * numberOfPancakes;
+
+            double sumPerDay = (priceCakaesPerDay + priceWafflesPerDay + pricePancakesPerDay) * numberOfBakers;
+            double sumForCampain = numberOfDays * sumPerDay;
+            double sumTotal = sumForCampain - (sumForCampain / 8);
+
+            Console.WriteLine(sumTotal);
+
+        }
+    }
+}
